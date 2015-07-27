eab295683a1626169d180b2112e90dad560310d7
Jan Sorgalla <jsorgalla@gmail.com>
2015-07-27 11:58:38 +0200

    Prepare release 1.0.1

--- bin/console.php
+++ bin/console.php
@@ -152,7 +152,7 @@ function store($name, array $data)
     );
 }
 
-$console = new Application('LocaleData', '1.0.0');
+$console = new Application('LocaleData', '1.0.1');
 $console
     ->register('generate')
     ->setDefinition([
