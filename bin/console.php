4fa496e37c38378d1916ea45d18842dcd8699a5b
Jan Sorgalla <jsorgalla@gmail.com>
2016-12-19 14:35:11 +0100

    Prepare release v1.2.1

--- bin/console.php
+++ bin/console.php
@@ -215,7 +215,7 @@ function process(array $localeData, $locale)
     return $data;
 }
 
-$console = new Application('LocaleData', '1.2.0');
+$console = new Application('LocaleData', '1.2.1');
 $console
     ->register('generate')
     ->setDefinition([
