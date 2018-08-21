6b3df12a912b7b2066ae2806e78a930b0bd41e92
Jan Sorgalla <jsorgalla@gmail.com>
2018-08-21 09:49:52 +0200

    Remove json_last_error_msg() polyfill

--- src/Reader/JsonReader.php
+++ src/Reader/JsonReader.php
@@ -24,7 +24,7 @@ class JsonReader implements ReaderInterface
         if (null === $data) {
             throw InvalidJsonException::create(
                 $fileName,
-                self::getLastJsonError()
+                json_last_error_msg()
             );
         }
 
@@ -34,27 +34,5 @@ class JsonReader implements ReaderInterface
     public function resolve($path, $locale)
     {
         return $path.'/'.$locale.'.json';
-    }
-
-    private static function getLastJsonError()
-    {
-        if (function_exists('json_last_error_msg')) {
-            return json_last_error_msg();
-        }
-
-        static $errors = array(
-            JSON_ERROR_NONE => null,
-            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
-            JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
-            JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
-            JSON_ERROR_SYNTAX => 'Syntax error, malformed JSON',
-            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded',
-        );
-
-        $error = json_last_error();
-
-        return array_key_exists($error, $errors)
-            ? $errors[$error]
-            : sprintf('Unknown error (%s)', $error);
     }
 }
